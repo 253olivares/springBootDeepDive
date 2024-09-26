8a1a4b6a5d6e3acb9c4f620a5fb480f9f8e0703a
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-09-26 13:27:51 -0400

    Added notes for annotations

--- bookkeeperApplication/src/main/java/com/bookkeeper/bookkeeperApplication/BookkeeperApplication.java
+++ bookkeeperApplication/src/main/java/com/bookkeeper/bookkeeperApplication/BookkeeperApplication.java
@@ -6,7 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// this starts up our spring boot applications. The spring boot app annotation triggers auto-configuration and component scanning
+// to search for beans that will be auto=loads into classes that require bean functionality. 
 @SpringBootApplication
+// used to specify this class works as a controller for our application. This
+// marks our class as a web request handler.
+// With controller spring knows to look for rest api request handlers within our
+// class.
 @Controller
 public class BookkeeperApplication {
 
@@ -14,7 +20,14 @@ public class BookkeeperApplication {
 		SpringApplication.run(BookkeeperApplication.class, args);
 	}
 
+	// we call request mapping to begin maping url requests to our application
+	// request mapping is a general use annotation just telling spring that this
+	// endpoint exist
+	// Spring doesnt know if it is a get post patch or delete request yet and have
+	// specific annotations for theses.
 	@RequestMapping("/")
+	// Response bodt automatically serializes our data into JSON. So Hello World
+	// string is passed back as a json object and printing to our page.
 	public @ResponseBody String greeting() {
 		return "Hello, World";
 	}
