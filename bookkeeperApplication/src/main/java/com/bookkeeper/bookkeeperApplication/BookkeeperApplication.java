d3e12e456a74bcd5b429fe404f814e7e942565dd
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2024-09-26 13:16:00 -0400

    Testing API
    - Testing API to return Hello World then url is localhost:8080/
    
    - passed
    
    - Added context to application properties to set a base url for the application.
    
    - Every endpoint should begin at localhost:8080/api/
    
    - pass

--- bookkeeperApplication/src/main/java/com/bookkeeper/bookkeeperApplication/BookkeeperApplication.java
+++ bookkeeperApplication/src/main/java/com/bookkeeper/bookkeeperApplication/BookkeeperApplication.java
@@ -2,12 +2,21 @@ package com.bookkeeper.bookkeeperApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class BookkeeperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookkeeperApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
 	}
 
 }
